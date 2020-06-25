a5dd80df65f867e2a4e3a76040ffbaa28309eb49
jp_darkarmour <jpchennai462@gmail.com>
2020-06-25 19:37:38 +0530

    query updates

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.prepare().then(() => {
             },
         }),
     );
-    server.use(graphQLProxy({version: ApiVersion.October19}))
+    server.use(graphQLProxy({version: ApiVersion.April20}))
     server.use(verifyRequest());
     server.use(async (ctx) => {
         await handle(ctx.req, ctx.res);
